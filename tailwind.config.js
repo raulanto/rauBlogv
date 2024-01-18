5e4debcf7628ea0c8cea6be163ee950be10edfa0
raul <74162376+raulanto@users.noreply.github.com>
2024-01-18 15:37:40 -0600

    instalacion y confg de tailwin

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}', 'index.html', './index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
